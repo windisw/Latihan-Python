c2268f4c0bda32da8c1d69e53cfd8be0c45b4394
Windi Solihatin Wahidah <61239275+windisw@users.noreply.github.com>
2026-04-01 13:06:49 +0700

    Add dictionary profile example in main_7.py

---
+++ main_7.py
@@ -0,0 +1,19 @@
+# Tipe data dictionary
+
+# %%
+profile_1 = {
+  "name": "Noval",
+  "is_male": False,
+  "age": 16,
+  "hobbies": ["gaming", "learning"]
+}
+
+print("name: %s" % (profile_1["name"]))
+print("hobbies: %s" % (profile_1["hobbies"]))
+print("age : %d" % (profile_1["age"]))
+print("is_male : %s" % (profile_1["is_male"]))
+#
+
+# %%
+profile_1 = { "name": "Noval", "hobbies": ["gaming", "learning"] }
+print(profile_1)
