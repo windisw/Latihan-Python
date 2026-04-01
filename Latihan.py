0fbe55a1c2144c015725745e10057182dade8ef7
Windi Solihatin Wahidah <61239275+windisw@users.noreply.github.com>
2026-04-02 04:35:39 +0700

    Add Python exercises with error explanations
    Added Python code examples with comments explaining errors and solutions.

---
+++ Latihan.py
@@ -0,0 +1,25 @@
+#NOMOR1
+harga = "15000"
+jumlah = 2
+print("Total belanja: " + harga * jumlah)
+#Kenapa kode di atas error, dan bagaimana cara memperbaikinya agar bisa menghitung total belanja?
+
+#NOMOR2
+belanja = ["beras", "gula", "kopi"]
+print(belanja[3])
+#Kenapa kode di atas error, dan bagaimana cara menampilkan item terakhir dari list dengan benar?
+
+#NOMOR3
+dompet = (50000, 20000, 10000)
+dompet[0] = 100000
+#Kenapa kode di atas error, dan bagaimana cara menampilkan uang terbesar di dompet?
+
+#NOMOR4
+barang_unik = {"beras", "gula", "kopi"}
+print(barang_unik[0])
+#Kenapa kode di atas error, dan bagaimana cara mengecek apa saja yang ada di dalam set?
+
+#NOMOR5
+profil_pembeli = {"nama": "Windi", "saldo": 100000}
+print("Alamat:", profil_pembeli["alamat","saldo"])
+#Kenapa kode di atas error, dan bagaimana cara menampilkan nama dan saldo bersebelahan?
